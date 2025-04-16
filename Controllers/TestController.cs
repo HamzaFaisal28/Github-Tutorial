7726c9aedf40b34a7444ae3b13b3610d42ba9ef7
Hira Faisal <hira.faisal7676@gmail.com>
2025-04-16 12:20:05 +0500

    Added function in Test Controller

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 namespace Github_Tutorial.Controllers
 {
     public class TestController : Controller
@@ -13,5 +12,12 @@ namespace Github_Tutorial.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public ActionResult Test()
+        {
+            return View();
+        }
+
     }
 }
